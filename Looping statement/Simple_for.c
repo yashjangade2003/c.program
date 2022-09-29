3a6816aea54cc221b906efa4bdad49c44db6bef5
yashjangade2003 <102903411+yashjangade2003@users.noreply.github.com>
2022-09-29 19:30:31 +0530

    Add files via upload

---
+++ Looping statement/Simple_for.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    for(i = 1; i <= 5; i++)
+    {
+        printf("\n\n INDIA");
+        printf("\n I LOVE MY INDIA");
+    }
+
+    printf("\n\n THANKS !!!");
+
+    getch();
+    return 0;
+}
+
