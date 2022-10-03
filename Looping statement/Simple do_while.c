232b3eaec032fb087472c188a1b1d0df619b6974
yashjangade2003 <102903411+yashjangade2003@users.noreply.github.com>
2022-10-03 21:16:51 +0530

    Add files via upload

---
+++ Looping statement/Simple do_while.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        printf("\n\n India");
+        printf("\n I love My india");
+        i++;
+    }
+    while(i <= 5);
+
+    printf("\n\n Thanks !!!");
+
+    getch();
+    return 0;
+}
