7179f1f1c04b86aaf1db5397b73365a273bd7d7a
yashjangade2003 <102903411+yashjangade2003@users.noreply.github.com>
2022-10-12 18:16:15 +0530

    Add files via upload

---
+++ Conditional control statment/Approch 1 Number Positive Or Negative_Using if else.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num = 0;
+
+    printf("\n Enter a Number = ");
+    scanf("%d",&Num);
+
+    if(Num > 0)
+    {
+        printf("\n %d is Positive Number.",Num);
+    }
+    else
+    {
+        printf("\n %d is Negative Number.",Num);
+    }
+
+    printf("\n\n Thanks !!!");
+
+    getch();
+    return 0;
+}
